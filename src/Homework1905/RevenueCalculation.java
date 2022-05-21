3f9d28ca2a8a0db260162b6f968f728b6693f08a
IvayloMetodiev <74488668+IvayloMetodiev@users.noreply.github.com>
2022-05-21 10:18:51 +0300

    Homework-1805-part2

---
+++ src/Homework1905/RevenueCalculation.java
@@ -0,0 +1,42 @@
+package Homework1905;
+
+import java.util.Scanner;
+
+public class RevenueCalculation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter an quantity:");
+        double quantity = scanner.nextDouble();
+        System.out.println("Please enter an unit price:");
+        double unitPrice = scanner.nextDouble();
+
+        double revenue = quantity * unitPrice;
+        double discount = 0;
+
+        if (quantity < 0.01 || unitPrice < 0.001){
+            System.out.println("Invalid input. Please enter only positive values");
+            System.exit(0);
+        }
+
+        if (quantity >= 100 && quantity <=120){
+            discount = 15;
+            revenue = revenue - (revenue * (discount/100));
+        } else if (quantity  > 120){
+            discount = 20;
+            revenue = revenue - (revenue * (discount/100));
+        }
+
+        System.out.println("You ordered:");
+        System.out.println();
+        System.out.printf("Quantity: %.0f", quantity);
+        System.out.println();
+        System.out.printf("Unite price : %.2f$", unitPrice);
+        System.out.println();
+        if (discount > 0){
+            System.out.printf("Discount: %.0f%%", discount);
+            System.out.println();
+        }
+        System.out.printf("Total order value: %.2f$", revenue);
+    }
+}
---
+++ src/Homework1905/WorkingAge.java
@@ -0,0 +1,20 @@
+package Homework1905;
+
+import java.util.Scanner;
+
+public class WorkingAge {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter your age:");
+        int age = scanner.nextInt();
+
+        if (age >= 16 && age <= 120) {
+            System.out.println("You are eligible to work.");
+        } else if (age < 16 && age >= 0){
+            System.out.println("You are NOT eligible to work.");
+        } else {
+            System.out.println("Your input is invalid, please try again.");
+        }
+    }
+}
